2dd6c48dcfb80afe58b90a55f2c3b9f1f1a438b8
hamza <hamzaensi@gmail.com>
2021-06-03 09:57:49 +0100

    final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,155 @@
 import 'package:flutter/material.dart';
+import 'quiz-brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: DicePage(),
-      ),
-    ),
-  );
-}
+QuizBrain quizBrain = QuizBrain();
+void main() => runApp(Quizzler());
 
-class DicePage extends StatelessWidget {
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundImage: AssetImage('images/hamza.jpg'),
-            radius: 60,
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
           ),
-          Text(
-            'Z Hamza',
-            style: TextStyle(
-              fontFamily: 'Pacifico',
-              color: Colors.white,
-              fontSize: 40.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            'Flutter developer',
-            style: TextStyle(
-              fontFamily: 'SourceSansPro',
-              color: Colors.teal.shade200,
-              fontSize: 25.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.5,
-            ),
-          ),
-          SizedBox(
-            width: 150.0,
-            height: 20.0,
-            child: Divider(
-              color: Colors.teal.shade100,
-            ),
-          ),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-            child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.teal,
-                ),
-                title: Text(
-                  '+216 22 632 153',
-                  style: TextStyle(
-                    color: Colors.teal.shade900,
-                    fontFamily: 'SourceSansPro',
-                    fontSize: 20.0,
-                  ),
-                )),
-          ),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-            child: ListTile(
-              leading: Icon(
-                Icons.email,
-                color: Colors.teal,
-              ),
-              title: Text(
-                'intwebser@gmail.com',
-                style: TextStyle(
-                  color: Colors.teal.shade900,
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
 }
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool? correctAnswer = quizBrain.getQuestionAnswer();
+
+    setState(() {
+      if (quizBrain.isFinished() == true) {
+        //TODO Step 4 Part A - show an alert using rFlutter_alert,
+
+        //This is the code for the basic alert from the docs for rFlutter Alert:
+        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
+
+        //Modified for our purposes:
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+
+        //TODO Step 4 Part C - reset the questionNumber,
+        quizBrain.reset();
+
+        //TODO Step 4 Part D - empty out the scoreKeeper.
+        scoreKeeper = [];
+      } else {
+        if (correctAnswer == userPickedAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+/*   List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  List<bool> answers = [false, true, true];
+  Question q1 = Question(q:'You can lead a cow down stairs but not up stairs.',a:false);
+  Question q2 = Question(q:'Approximately one quarter of human bones are in the feet.',a:true);
+  Question q3 = Question(q:'A slug\'s blood is green.',a:true); */
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText().toString(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              child: Container(
+                color: Colors.green,
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              child: Container(
+                color: Colors.red,
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Row(children: scoreKeeper),
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
---
+++ lib/quiz-brain.dart
@@ -0,0 +1,47 @@
+import 'question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+  List<Question> _questionBank = [
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true)
+  ];
+
+  void nextQuestion() {
+    if (_questionNumber < _questionBank.length - 1) {
+      _questionNumber++;
+    }
+  }
+
+  bool? endQuestion() {
+    if (_questionNumber + 1 < _questionBank.length)
+      return false;
+    else
+      return true;
+  }
+
+  String? getQuestionText() {
+    return _questionBank[_questionNumber].questionText;
+  }
+
+  bool? getQuestionAnswer() {
+    return _questionBank[_questionNumber].questionAnswer;
+  }
+
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
+
+      print('Now returning true');
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //TODO: Step 4 part B - Create a reset() method here that sets the questionNumber back to 0.
+  void reset() {
+    _questionNumber = 0;
+  }
+}
